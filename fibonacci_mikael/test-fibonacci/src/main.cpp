90a5fb5dec629eea3afc633d9e3bc22b0663dd1e
MPerrot <mik.perrot778@outlook.fr>
2020-03-07 15:51:47 -0500

    inital window setup

--- fibonacci_mikael/test-fibonacci/src/main.cpp
+++ fibonacci_mikael/test-fibonacci/src/main.cpp
@@ -5,6 +5,7 @@
 #include "imgui.h"
 #include "imgui_impl_glfw.h"
 #include "imgui_impl_opengl3.h"
+#include "fibonacci.h"
 #include <stdio.h>
 
 // About Desktop OpenGL function loaders:
@@ -114,11 +115,17 @@ int main(int, char**)
     //IM_ASSERT(font != NULL);
 
     // Our state
-    bool show_demo_window = true;
-    bool show_another_window = false;
+    static int values[2];
+    static bool sim_start = false;
+    ImVec2 fibWin_size = ImVec2(500.0,300.0);
+    ImVec2 fibWin_pos = ImVec2(0.0,0.0);
+    ImVec2 simWin_size = ImVec2(500.0, 300.0);
+    ImVec2 simWin_pos = ImVec2(500, 0.0);
+
     ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 
     // Main loop
+
     while (!glfwWindowShouldClose(window))
     {
         // Poll and handle events (inputs, window resize, etc.)
@@ -131,42 +138,45 @@ int main(int, char**)
         // Start the Dear ImGui frame
         ImGui_ImplOpenGL3_NewFrame();
         ImGui_ImplGlfw_NewFrame();
+
         ImGui::NewFrame();
 
-        // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
-        if (show_demo_window)
-            ImGui::ShowDemoWindow(&show_demo_window);
+        ImGui::SetNextWindowPos(fibWin_pos, ImGuiCond_Once);
+        ImGui::SetNextWindowSize(fibWin_size, ImGuiCond_Once);
 
-        // 2. Show a simple window that we create ourselves. We use a Begin/End pair to created a named window.
-        {
-            static float f = 0.0f;
-            static int counter = 0;
+        ImGui::Begin("Fibonnaci");                         
+        if (!sim_start) {
+            ImGui::Text("Values to generate simulation (Must be greater than zero)");
+            ImGui::InputInt2("", values);
+            ImGui::Text("Valeur 1: %d", values[0]);
+            ImGui::Text("Valeur 2: %d", values[1]);
 
-            ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
+            if (values[0] < 0 || values[1] < 0) {
+                values[0] = 0;
+                values[1] = 0;
+            }
 
-            ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
-            ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
-            ImGui::Checkbox("Another Window", &show_another_window);
-
-            ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
-            ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
-
-            if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
-                counter++;
-            ImGui::SameLine();
-            ImGui::Text("counter = %d", counter);
-
-            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-            ImGui::End();
+            if (values[0] > 0 && values[1] > 0) {
+                if (ImGui::Button("Jouer")) {
+                    sim_start = true;
+                }
+            }
         }
+        else{
+            ImGui::Text("Simulation is running");
+            if (ImGui::Button("Stop simulation")) {
+                sim_start = false;
+            }
+        }
+        ImGui::End();
+        
+        if (sim_start) {
+            ImGui::SetNextWindowPos(simWin_pos, ImGuiCond_Appearing);
+            ImGui::SetNextWindowSize(simWin_size, ImGuiCond_Appearing);
+            ImGui::Begin("Simulation");
+            ImGui::Text("Test");
 
-        // 3. Show another simple window.
-        if (show_another_window)
-        {
-            ImGui::Begin("Another Window", &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
-            ImGui::Text("Hello from another window!");
-            if (ImGui::Button("Close Me"))
-                show_another_window = false;
+
             ImGui::End();
         }
 
